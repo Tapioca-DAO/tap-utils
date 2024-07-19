6d0dd41c715bd96940e1aef833600a80b6da5657
Elpacos <vicroc.vn@gmail.com>
2024-07-20 01:16:52 +0200

    isolated utils from magnetar, removed dependencies

--- contracts/tapioca-yieldbox-loader.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.22;
-
-/// THIS FILE IS USED TO LOAD THE TAPIOCA YIELDBOX CONTRACTS
-/// Comment the imports for faster compilation
-
-import {YieldBoxURIBuilder} from "yieldbox/YieldBoxURIBuilder.sol";
-import {YieldBox} from "yieldbox/YieldBox.sol";
